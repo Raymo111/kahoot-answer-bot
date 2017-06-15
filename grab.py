c60e109fbcdcf39c4b798f688865564a8c909ac3
Peter Stenger <peter.a.stenger@gmail.com>
2017-06-15 10:23:04 -0400

    fixes #5

--- grab.py
+++ grab.py
@@ -9,51 +9,62 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 speed = 0
 #-------------------------------------------------------------------------#
-def get_page(id, email, passwd,speed):
+def get_page(id, email, passwd):
+    speed = 0
     driver = webdriver.Chrome()
-    driver.get('https://create.kahoot.it/#quiz/' + id);
-    time.sleep(0.25 + speed)
-    box = driver.find_element_by_css_selector('#username-input-field__input')
-    box.send_keys(email)
-    box2 = driver.find_element_by_css_selector('#password-input-field__input')
-    box2.send_keys(passwd)
-    driver.find_element_by_css_selector('.button--cta-play').click()
-    time.sleep(2 + speed)
-    elem = driver.find_element_by_xpath("//*")
-    stuff = elem.get_attribute("innerHTML")
+    while True:
+        driver.get('https://create.kahoot.it/#quiz/' + id);
+        time.sleep(0.25 + speed)
+        box = driver.find_element_by_css_selector('#username-input-field__input')
+        box.send_keys(email)
+        box2 = driver.find_element_by_css_selector('#password-input-field__input')
+        box2.send_keys(passwd)
+        driver.find_element_by_css_selector('.button--cta-play').click()
+        time.sleep(2 + speed)
+        elem = driver.find_element_by_xpath("//*")
+        stuff = elem.get_attribute("innerHTML")
+        try:
+            driver.find_element_by_css_selector('.kahoot-registration') #still on login
+            speed += 2.5
+            print('Retrying with speed set to {}'.format(speed))
+
+        except Exception:
+            break
     try:
         driver.find_element_by_css_selector(".create-kahoot-type-selector")
         print("Private kahoot.")
         exit()
     except Exception:
         driver.quit()
+        
+        
     return stuff
 #-------------------------------------------------------------------------#
-def start_bot(id,name,answers,speed):
+def start_bot(id,name,answers,speed=0):
     driver = webdriver.Chrome()
-    driver.get("https://kahoot.it/#/")
-    time.sleep(0.25 + speed)
-    box = driver.find_element_by_css_selector('#inputSession')
-    box.send_keys(id)
-    driver.find_element_by_css_selector('.btn-greyscale').click()
-    time.sleep(1 + speed)
-    box = driver.find_element_by_css_selector('#username')
-    box.send_keys(name)
-    driver.find_element_by_css_selector('.btn-greyscale').click()
-    response = input("click [ENTER] to start the bot\npress n + [ENTER] to choose a new name and anything else to retry login.\n")
-    if response == 'n':
-        driver.quit()
-        name = input('New name > ')
-        print('Retrying with name set to {}'.format(name))
-        start_bot(id,name,answers,speed)
-    elif response == '': 
+    while True:
+        try:
+            driver.get("https://kahoot.it/#/")
+            time.sleep(0.25 + speed)
+            box = driver.find_element_by_css_selector('#inputSession')
+            box.send_keys(id)
+            driver.find_element_by_css_selector('.btn-greyscale').click()
+            time.sleep(1.25 + speed)
+            box = driver.find_element_by_css_selector('#username')
+            box.send_keys(name)
+            driver.find_element_by_css_selector('.btn-greyscale').click()
+            break
+        except Exception:
+            speed += 2.5
+            print('Retrying with delay set to {} seconds'.format(speed))
+    response = input("click [ENTER] to start the bot\npress anything + [ENTER] to choose a new name\n")
+    if response == '': 
         bot_answer(driver,answers)
     else:
         driver.quit()
-        speed += 2.5
-        print('Retrying with speed set to {}'.format(speed))
-        start_bot(id,name,answers,speed)
-
+        name = input('New name > ')
+        print('Retrying with name set to {} and delay set to {}'.format(name,speed))
+        start_bot(id,name,answers,speed=speed)
 #-------------------------------------------------------------------------#
 def bot_answer(driver,answers):
     lookuptable = {"0":".answerA", "1":".answerB","2":".answerC","3":".answerD"}
@@ -115,28 +126,17 @@ def getAnswers(soup,hascolor=True):
     return colors, answers
 #-------------------------------------------------------------------------#
 def printAnswers(url,email,passwd,co,co2,co3):
-    global speed
-    html = get_page(url,email,passwd,speed)
+    html = get_page(url,email,passwd)
     soup = BeautifulSoup(html, 'html.parser')
     questions = getQuestions(soup)
-    
-    if questions == []:
-        speed += 2.5
-        print('failed to reach page, wifi not fast enough. Retrying with delay set to {} seconds.'.format(speed))
-        printAnswers(url,email,passwd,co,co2,co3)
     colors, answers = getAnswers(soup)
     for i in range(len(questions)):
         print('{}{:100s}{}  |  {}{:6s}{} |  {}{:3d}{}  |'.format(co,questions[i],co2,co,colors[i],co2,co,i+1,co2))
         print('{}{:100s}{}  |         |       |'.format(co3,answers[i],co2))
 #-------------------------------------------------------------------------#
 def scrape(url,email,passwd):
-    global speed
-    html = get_page(url,email,passwd,speed)
+    html = get_page(url,email,passwd)
     soup = BeautifulSoup(html, 'html.parser')
     answers, asd = getAnswers(soup,hascolor=False)
-    if answers == []:
-        speed += 2.5
-        print('failed to reach page, wifi not fast enough. Retrying with delay set to {} seconds.'.format(speed))
-        scrape(url,email,passwd)
     return answers
 #-------------------------------------------------------------------------#
