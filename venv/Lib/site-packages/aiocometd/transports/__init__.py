0b024de342fe92f83b9ee0d0658ae7ca1069eeea
Raymond Li <hi@raymond.li>
2021-06-17 20:14:52 -0400

    Remove venv

--- venv/Lib/site-packages/aiocometd/transports/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Transport classes and functions"""
-from aiocometd.transports.registry import create_transport  # noqa: F401
-from aiocometd.transports import long_polling, websocket  # noqa: F401
